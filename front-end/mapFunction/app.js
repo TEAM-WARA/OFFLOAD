785130a06f926d9b0b0b33a500eeccccb6c1d7d3
ryusemin <tpals3799@naver.com>
2023-10-27 17:47:47 +0900

    ✅ 반경 내 가게좌표 불러오기 + 검색 기능

--- front-end/mapFunction/app.js
+++ front-end/mapFunction/app.js
@@ -2,17 +2,120 @@ const { kakao } = window;
 
 var mapDiv = document.querySelector('#map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(35.8678658, 128.5967954), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(35.856047838165004, 128.49278206824263), // 지도의 중심좌표 (35.8678658, 128.5967954)
         level: 3 // 지도의 확대 레벨
     };
 
-var map = new kakao.maps.Map(mapDiv, mapOption);
+var map = new kakao.maps.Map(mapDiv, mapOption);  // 지도를 생성합니다
+const ps = new kakao.maps.services.Places(); // 장소 검색 객체 생성
+
+var markers = [];
 
 
 mapDiv.style.width = '70%';   //MapSize 조절
 mapDiv.style.height = '70vh';
 
 map.relayout();
+
+
+// 마커 정보를 담은 배열
+var markerList = []
+
+var circle = new kakao.maps.Circle({
+    center : new kakao.maps.LatLng(35.856047838165004, 128.49278206824263),
+    radius: 100,
+    strokeWeight: 5, // 선의 두께입니다 
+    strokeColor: '#75B8FA', // 선의 색깔입니다
+    strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: 'dashed', // 선의 스타일 입니다
+    fillOpacity: 0  // 채우기 불투명도 입니다 
+
+}); 
+var centerAround = circle.getBounds();
+console.log(centerAround);
+
+
+// centerAround의 남서쪽과 북동쪽 좌표를 가져옵니다
+var swLatLng = centerAround.getSouthWest();
+var neLatLng = centerAround.getNorthEast();
+
+var circleXY = {
+    "minX" : swLatLng.getLng(),
+    "minY" : swLatLng.getLat(),
+    "maxX" : neLatLng.getLng(),
+    "maxY" : neLatLng.getLat(),
+};
+
+var prevLatlng// 이전 중심 좌표를 저장할 변수
+
+// 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'center_changed', function() {
+
+    // 지도의  레벨을 얻어옵니다
+    var level = map.getLevel();
+
+    // 지도의 중심좌표를 얻어옵니다 
+    var latlng = map.getCenter();   
+
+
+    circle.setPosition(latlng);
+    circle.setRadius(level * 1000);
+    circle.setMap(map);
+
+    // 이전 중심 좌표가 있고, 새로운 중심 좌표와의 차이가 0.001 미만이면 AJAX 요청을 보내지 않습니다
+    if (prevLatlng && Math.abs(prevLatlng.getLat() - latlng.getLat()) < 0.1 && Math.abs(prevLatlng.getLng() - latlng.getLng()) < 0.1) {
+        return;
+    }
+    // 새로운 중심 좌표를 이전 중심 좌표로 저장합니다
+    prevLatlng = latlng;
+
+    var centerAround = circle.getBounds();
+    console.log(centerAround);
+
+    swLatLng = centerAround.getSouthWest();
+    neLatLng = centerAround.getNorthEast();
+
+    circleXY = {
+    "minX" : swLatLng.getLng(),
+    "minY" : swLatLng.getLat(),
+    "maxX" : neLatLng.getLng(),
+    "maxY" : neLatLng.getLat(),
+};
+
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }            
+
+    // markers 배열 초기화
+    markers = [];
+
+var BodyJson = JSON.stringify(circleXY);
+
+    $.ajax({
+        type: 'POST',
+        headers:{
+            "Content-type": "application/json; charset=utf-8",
+            "Authorization" : "access token",
+        },
+        url: "https://port-0-creativefusion-jvpb2aln5qmjmz.sel5.cloudtype.app/store/range",
+        data: BodyJson,
+        success: function(data){
+            // 서버에서 받은 데이터를 markerList에 저장
+            markerList = data;
+            console.log("데이터 전송 완료");
+            console.log(markerList);
+
+            initKakaoMap();
+        },
+        error: function(request, status, error,body) {
+            console.log(request);
+            console.log(body);
+            console.log(error);
+        }
+    });  
+
+});
+
 
 let currentPopup = null; // 현재 열려있는 팝업 정보를 저장하는 변수, boolean
 
@@ -36,52 +139,63 @@ function createMarkerImage(markerScr, markerSize) {
     normalImage = createMarkerImage(imageSrc2, imageSize);
 
 
-// 마커 정보를 담은 배열
-var markerList = [
-    { "lat" : 35.8678658, "lon": 128.5967954, "Shopping": true,
-    "place": "빨간바지", // 상점위치 string
-    "address" : "대구 중구 동성로2길 48", // 주소 string
-    "storeItem" : "장바구니 물품 존재 - 아우터 외 2개" // 물건 string
-  },
-    { "lat": 35.8709820, "lon": 128.5926346, "Shopping": true,
-    "place": "한우장설렁탕", // 상점위치 string
-    "address" : "대구 중구 국채보상로 567", // 주소 string
-    "storeItem" : "장바구니 물품 존재 - 설렁탕" // 물건 string
-   },
-    { "lat": 35.8704173, "lon": 128.5953787, "Shopping": false,
-    "place": "CGV 대구한일", // 상점위치 string
-    "address" : "대구광역시 중구 동성로 39", // 주소 string
-    "storeItem" : "장바구니 물품 없음" // 물건 string
-  }
-  ],
   selectedMarker = null; // 클릭한 마커를 담을 변수
 
-// 지도에 마커를 표시하는 함수입니다
-function displayMarker(place) {
-    
-// 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x) 
-    });
 
+searchPlace = (place)=>{
+    // 키워드로 장소를 검색합니다
+    ps.keywordSearch("대구" + place, placesSearchCB); 
+    }
+
+
+  // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+  function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<1; i++) {  
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
 }
+
+
+// 검색창 값 변환
+function myFunction(event) {
+    event.preventDefault();
+    var searchValue = document.getElementById("searchBox").value;
+    document.getElementById("result").innerHTML = "You entered: " + searchValue;
+    searchPlace(searchValue);
+  }
+
+
+
+
 
 function initKakaoMap() {
 
 // InfoWindow 객체들의 참조를 저장하는 배열
 var infowindowList = [];
 
+
 markerList.forEach(function(markerInfo) {
-    // 마커를 생성합니다
     
+    // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(markerInfo.lat, markerInfo.lon),
+        position: new kakao.maps.LatLng(markerInfo.coordinateY, markerInfo.coordinateX),
         map: map,
         image: normalImage, // 마커 이미지
     });
         marker.normalImage = normalImage;
         marker.clickImage = clickImage;
+
+    markers.push(marker);
 
     // Popup창 정보, 디자인
     var popupContent =`
@@ -91,11 +205,11 @@ markerList.forEach(function(markerInfo) {
       </div>
       <div class='info-box'>
         <p style='margin-bottom: 7px; overflow: hidden;'>
-          <span class='tit'>${markerInfo.place}</span>
+          <span class='tit'>${markerInfo.name}</span>
           <a href='/' target='_blank' class='link' style='color: #3D6DCC; font-size: 13px; float: right;'>가게이동</a>
         </p>
         <p><span class='new-addr'>${markerInfo.address}</span></p>
-        <p><span class='old-addr'>${markerInfo.storeItem}</span></p>
+        <p><span class='old-addr'>${markerInfo.id}</span></p>
       </div>
     </div>`;  
 
@@ -106,6 +220,7 @@ markerList.forEach(function(markerInfo) {
     });
 
     infowindowList.push(infoWindow);
+
 
 
 //Popup창 켜고 끄는 method
@@ -164,16 +279,11 @@ function removePopup(){
         showPopup(markerInfo, popupContent);
 
         // 지도 중심을 클릭된 마커 위치로 이동
-        map.panTo(new kakao.maps.LatLng(markerInfo.lat, markerInfo.lon));
+        map.panTo(new kakao.maps.LatLng(markerInfo.coordinateY, markerInfo.coordinateX));
     });
 
 
-        
 });
-
-
-
-
 
 
 }
